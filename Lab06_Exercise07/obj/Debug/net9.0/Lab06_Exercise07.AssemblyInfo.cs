b53695937f495eb6aea646222ba8134f3951aa90
Thesiri04 <sirirungnapa201@gmail.com>
2025-01-13 23:49:11 +0700

    commit07

--- Lab06_Exercise07/obj/Debug/net9.0/Lab06_Exercise07.AssemblyInfo.cs
+++ Lab06_Exercise07/obj/Debug/net9.0/Lab06_Exercise07.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab06_Exercise07")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99ad4789dc9c4b8c7591e09ddf98bf977159e50d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94ca5bf1a96af553abcdd8d841b0a52775bb1cb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab06_Exercise07")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab06_Exercise07")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
