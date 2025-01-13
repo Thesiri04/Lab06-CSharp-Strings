94ca5bf1a96af553abcdd8d841b0a52775bb1cb8
Thesiri04 <sirirungnapa201@gmail.com>
2025-01-13 23:45:45 +0700

    LAB6-Exercise

---
+++ Lab06_Exercise01/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+System.Console.WriteLine("Hello, I'm 66030188");
+   System.Console.WriteLine("Please enter your name");
+   var name = System.Console.ReadLine();  
+   System.Console.WriteLine($"Your name is {name}");
